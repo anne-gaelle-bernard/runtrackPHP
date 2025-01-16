52f29516b7e60737df5078d5be00bd4e48e94739
anne-gaelle-bernard <anne-gaelle.bernard@laplateforme.io>
2025-01-16 16:57:20 +0100

    runtrack

--- jour4/job1/index.php
+++ jour4/job1/index.php
@@ -22,23 +22,9 @@
 </html>
 
 <?php
-// Activer  rapport d'erreurs
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-// Vérifier si $_GET contient des données
-if (!empty($_GET)) {
-    // Compter le nombre d'arguments dans $_GET
-    $nombre_arguments = count($_GET);
 
-    // Afficher le résultat
-    echo "Nombre d'arguments reçus : " . htmlspecialchars($nombre_arguments) . "<br>";
 
-    //  Afficher les arguments pour vérifier
-    echo "<pre>";
-    print_r(array_map('htmlspecialchars', $_GET)); // Montre les clés et les valeurs
-    echo "</pre>";
-} else {
-    echo "Aucun argument reçu.";
-}
+   
+    echo "Nombre d'arguments reçus : " . count($_GET) . "<br>";
 ?>
